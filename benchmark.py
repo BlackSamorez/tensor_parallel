f1163eec17aacf10360929cce7f208353babf214
Iaroslav Lisniak <48571134+IaroslavLisniak@users.noreply.github.com>
2022-10-09 22:39:23 +0100

    Updated benchmark.py
    Added command line arguments: batch_size, do_backward, num_iter
    Changed datetime to perf_count

--- benchmark.py
+++ benchmark.py
@@ -11,8 +11,7 @@ from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from torchvision.datasets import MNIST
 
-from datetime import datetime
-
+import time
 
 BACKEND = 'nccl' if torch.cuda.is_available() else 'gloo'
 
@@ -52,14 +51,39 @@ class Net(nn.Module):
         return output
 ##############################################################
 
+# default values.
+DO_BACKWARD: int = True
+NUM_ITER: int = 20
+BATCH_SIZE: int = 1000
+
+    
+# Arg parse ################################################### 
+import getopt, sys
+
+argumentList = sys.argv[1:]
+options = "d:n:b:"
+long_options = ["do_backward=", "num_iter=", "batch_size="]
+arguments, values = getopt.getopt(argumentList, options, long_options)
+
+for currentArgument, currentValue in arguments:
+        if currentArgument in ("-d", "--do_backward"):
+            DO_BACKWARD = bool(currentValue)      
+        elif currentArgument in ("-n", "--num_iter"):
+            NUM_ITER = int(currentValue)
+        elif currentArgument in ("-b", "--batch_size"):
+            BATCH_SIZE = int(currentValue)
+
+# print(f"Benchmark settings:")
+# print(f"DO_BACKWARD: {DO_BACKWARD}\nNUM_ITER: {NUM_ITER}\nBATCH_SIZE: {BATCH_SIZE}")
+##############################################################
 
 def run_training(rank, size):
     torch.manual_seed(1234)
     device = torch.device('cuda', rank) if torch.cuda.is_available() else torch.device('cpu')
 
     # Data generating process. ###########################
-    data = torch.randn(100, 1, 28, 28).to(device)
-    target = torch.randint(0, 10, (100,)).to(device)
+    data = torch.randn(BATCH_SIZE, 1, 28, 28).to(device)
+    target = torch.randint(0, 10, (BATCH_SIZE,)).to(device)
     ######################################################
 
 
@@ -72,23 +96,37 @@ def run_training(rank, size):
     steps = 0
     epoch_loss = 0
 
-    start_time = datetime.now()
-    for iter in range(1000):
+    if DO_BACKWARD:
+        start_time = time.perf_counter_ns()
+        for iter in range(NUM_ITER):
 
-        optimizer.zero_grad()
-        output = model(data)
-        loss = torch.nn.functional.cross_entropy(output, target)
-        epoch_loss += loss.item()
-        loss.backward()
+            optimizer.zero_grad()
+            output = model(data)
+            loss = torch.nn.functional.cross_entropy(output, target)
+            epoch_loss += loss.item()
+            loss.backward()
 
-        optimizer.step()
+            optimizer.step()
 
-    if torch.cuda.is_available():
-        torch.cuda.synchronize(device)
+        if torch.cuda.is_available():
+            torch.cuda.synchronize(device)
 
-    working_time = datetime.now() - start_time
+        working_time = time.perf_counter_ns() - start_time
+    else:
+        start_time = time.perf_counter_ns()
+        for iter in range(NUM_ITER):
+            output = model(data)
 
-    print(f"Rank {rank} worked for {working_time}")
+            # Do we need loss calculation?
+            loss = torch.nn.functional.cross_entropy(output, target)
+            epoch_loss += loss.item()
+
+        if torch.cuda.is_available():
+            torch.cuda.synchronize(device)
+
+        working_time = time.perf_counter_ns() - start_time
+
+    print(f"Rank {rank} worked for {working_time / 1e9:,.2f} seconds; ")
 
 if __name__ == "__main__":
     local_rank = int(os.environ["LOCAL_RANK"])
