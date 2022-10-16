5f702875abf8311fb6a774936fff3aafcbdc116e
Iaroslav Lisniak <yalisnyak@nes.ru>
2022-10-16 12:51:11 +0100

    Started new benchmark

---
+++ markbench.py
@@ -0,0 +1,125 @@
+import os
+import time
+
+import torch
+import torch.distributed as dist
+from   transformers.models.bloom.configuration_bloom import BloomConfig
+
+from parallel_blocks import ParallelBlock, ParallelMLP, ParallelAttention
+
+
+# Parallel settings ###########################################
+BACKEND = 'nccl' if torch.cuda.is_available() else 'gloo'
+torch.set_num_threads(1)
+
+def init_process(local_rank, fn, backend=BACKEND):
+    """ Initialize the distributed environment. """
+    dist.init_process_group(backend, rank=local_rank)
+    size = dist.get_world_size()
+    fn(local_rank, size)
+    
+# Arg parse ################################################### 
+import getopt, sys
+
+# default values.
+DO_BACKWARD: int = False
+NUM_ITER: int    = 100
+BATCH_SIZE: int  = 4
+SEQ_LENGTH: int  = 17
+MAXIMUM: bool    = False
+
+argumentList = sys.argv[1:]
+options = "d:n:b:m:s"
+long_options = ["do_backward=", "num_iter=",
+                "batch_size=", "maximum",
+                "seq_length=",]
+
+arguments, values = getopt.getopt(argumentList, options, long_options)
+for currentArgument, currentValue in arguments:
+        if currentArgument in ("-d", "--do_backward"):
+            DO_BACKWARD = bool(currentValue)      
+        elif currentArgument in ("-n", "--num_iter"):
+            NUM_ITER = int(currentValue)
+        elif currentArgument in ("-b", "--batch_size"):
+            BATCH_SIZE = int(currentValue)
+        elif currentArgument in ("-m", "--maximum"):
+            MAXIMUM = True
+        elif currentArgument in ("-s", "--seq_length"):
+            SEQ_LENGTH = int(currentValue)
+
+print(f"Benchmark setting:")
+print(f"DO_BACKWARD: {DO_BACKWARD}")
+print(f"NUM_ITER:    {NUM_ITER}")
+print(f"BATCH_SIZE:  {BATCH_SIZE}")
+print(f"MAXIMUM:     {MAXIMUM}")
+print(f"SEQ_LENGTH:  {SEQ_LENGTH}")
+
+##############################################################
+
+def run_training(rank, size):
+    torch.manual_seed(1234)
+    device = torch.device('cuda', rank) if torch.cuda.is_available() else torch.device('cpu')
+
+    # Data generating process. ###########################
+    config = BloomConfig()
+    if MAXIMUM:
+        config = config.from_pretrained("bigscience/bloom")
+
+    data = torch.randn(BATCH_SIZE, SEQ_LENGTH, config.hidden_size).to(device)
+    target = torch.rand((BATCH_SIZE, SEQ_LENGTH, config.hidden_size)).to(device)
+    ######################################################
+
+
+    # Your model here. ###################################
+    model = ParallelMLP(config).to(device)
+    ######################################################
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+    steps = 0
+    epoch_loss = 0
+    # DO_BACKWARD = False  # why do we need that crunch?
+    if torch.distributed.get_world_size() > 1:
+        torch.distributed.barrier()
+    with torch.cuda.amp.autocast():
+        if DO_BACKWARD:
+            start_time = time.perf_counter_ns()
+            for iter in range(NUM_ITER):
+
+                optimizer.zero_grad()
+                output = model(data)
+                loss = torch.nn.functional.mse_loss(output, target)
+                epoch_loss += loss.item()
+                loss.backward()
+
+                optimizer.step()
+
+            if torch.cuda.is_available():
+                torch.cuda.synchronize(device)
+
+            working_time = time.perf_counter_ns() - start_time
+        else:
+            with torch.no_grad():
+                for iter in range(100):
+                    output = model(data, data)
+                if torch.cuda.is_available():
+                    torch.cuda.synchronize(device)
+                if torch.distributed.get_world_size() > 1:
+                    torch.distributed.barrier()
+
+                start_time = time.perf_counter_ns()
+                for iter in range(NUM_ITER):
+                    output = model(data, data)
+
+                    loss = torch.nn.functional.cross_entropy(output, target)
+                    epoch_loss += loss.item()
+
+                if torch.cuda.is_available():
+                    torch.cuda.synchronize(device)
+
+                working_time = time.perf_counter_ns() - start_time
+
+    print(f"Mean Iter time for Rank {rank}: {working_time / 1e6 / NUM_ITER:,.2f} ms; ")
+
+if __name__ == "__main__":
+    local_rank = int(os.environ["LOCAL_RANK"])
+    init_process(local_rank, fn=run_training, backend=BACKEND)
