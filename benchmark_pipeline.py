5842a7da99c30f68e23f9510739b0a52d708b378
Andrei Panferov <andrei@blacksamorez.ru>
2022-10-22 13:29:11 +0000

    half

--- benchmark_pipeline.py
+++ benchmark_pipeline.py
@@ -13,23 +13,24 @@ from torchgpipe import GPipe
 import getopt, sys
 
 # default values.
-TRAIN:       int  = False
+TRAIN:       bool = False
 NUM_ITER:    int  = 100
 BATCH_SIZE:  int  = 128
 SEQ_LENGTH:  int  = 17
 BLOOMCONFIG: str  = "bloom-7b1"
 DEPTH:       int  = 2
+HALF:        bool = False
 
 argumentList = sys.argv[1:]
-options = "t:n:b:s:c:d:p:"
-long_options = ["train=", "num_iter=",
+options = "t:n:b:s:c:d:p:h:"
+long_options = ["train", "num_iter=",
                 "batch_size=", "seq_length=", 
-                "bloomconfig=", "depth="]
+                "bloomconfig=", "depth=", "half"]
 
 arguments, values = getopt.getopt(argumentList, options, long_options)
 for currentArgument, currentValue in arguments:
         if currentArgument in ("-t", "--train"):
-            TRAIN = int(currentValue)      
+            TRAIN = True      
         elif currentArgument in ("-n", "--num_iter"):
             NUM_ITER = int(currentValue)
         elif currentArgument in ("-b", "--batch_size"):
@@ -40,6 +41,8 @@ for currentArgument, currentValue in arguments:
             BLOOMCONFIG = currentValue
         elif currentArgument in ("-d", "--depth"):
             DEPTH = int(currentValue)
+        elif currentArgument in ("-h", "--half"):
+            HALF = True
 
 print(f"Benchmark setting:")
 print(f"TRAIN: {TRAIN}")
@@ -73,15 +76,22 @@ def run_training():
         model = GPipe(nn.Sequential(*[MiddleBloom(config) for _ in available_gpus]),
                     balance=[1 for _ in available_gpus],
                     devices=available_gpus,
-                    chunks=8)
+                    chunks=BATCH_SIZE // 8)
 
         in_device = model.devices[0]
         out_device = model.devices[-1]
+
+    if HALF:
+        model = model.half()
     ######################################################
 
     # Data generating process. ###########################
     data = torch.randn(BATCH_SIZE, SEQ_LENGTH, config.hidden_size).to(in_device)
     target = torch.rand((BATCH_SIZE, SEQ_LENGTH, config.hidden_size)).to(out_device)
+
+    if HALF:
+        data = data.half()
+        target = target.half()
     ######################################################
 
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
@@ -97,7 +107,7 @@ def run_training():
                  output = model(data)
 
             if torch.cuda.is_available():
-                 torch.cuda.synchronize(device)
+                 torch.cuda.synchronize(out_device)
 
             for iter in range(NUM_ITER):
 
