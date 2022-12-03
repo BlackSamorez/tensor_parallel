2af36b6b68eb75e5bc7ec60660ae069c7cebc79b
Andrei Panferov <andrei@blacksamorez.ru>
2022-12-03 13:58:40 +0000

    CPU for cpu test

--- petals_local_parallel/slicer_wrapper_test_cpu.py
+++ petals_local_parallel/slicer_wrapper_test_cpu.py
@@ -20,7 +20,7 @@ def converter_main():
     target_output = model(test_input).last_hidden_state
 
     print(f"Loading slices")
-    model = tensor_parallel(MODEL_CLS, devices=["cuda:0", "cuda:1"]).from_pretrained(NAME)
+    model = tensor_parallel(MODEL_CLS, devices=["cpu", "cpu", "cpu", "cpu"]).from_pretrained(NAME)
     model.scatter()
     
     sharded_output = model(test_input).last_hidden_state.cpu()
