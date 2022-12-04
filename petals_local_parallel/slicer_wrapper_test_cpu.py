2bdc52a979ea2fc45b04a4bc090d41b8cb89f832
Iaroslav Lisniak <yalisnyak@nes.ru>
2022-12-04 12:12:33 +0000

    reduce num devices

--- petals_local_parallel/slicer_wrapper_test_cpu.py
+++ petals_local_parallel/slicer_wrapper_test_cpu.py
@@ -22,7 +22,7 @@ def converter_main():
     target_output = model(test_input).last_hidden_state
 
     print(f"Loading slices")
-    model = tensor_parallel(MODEL_CLS, devices=["cpu", "cpu", "cpu", "cpu"]).from_pretrained(NAME)
+    model = tensor_parallel(MODEL_CLS, devices=["cpu", "cpu"]).from_pretrained(NAME)
     model.scatter()
     
     sharded_output = model(test_input).last_hidden_state.cpu()
