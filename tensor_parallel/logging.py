f1d3c4fbc206fb7f9dd22e3c24914709cbbcda69
justheuristic <justheuristic@gmail.com>
2022-12-14 13:26:43 +0300

    Refactoring
    Co-authored-by: Iaroslav Lisniak <48571134+IaroslavLisniak@users.noreply.github.com>
    Co-authored-by: Iaroslav Lisniak <yalisnyak@nes.ru>
    Co-authored-by: Andrei Panferov <andrei@blacksamorez.ru>
    Co-authored-by: Andrei Panferov <andrei.panferov@eqvilent.com>

---
+++ tensor_parallel/logging.py
@@ -0,0 +1,194 @@
+"""
+Fancy colored loggging, borrowed from https://github.com/learning-at-home/hivemind/
+All credit goes to Alexander Borzunov https://github.com/borzunov
+"""
+import logging
+import os
+import sys
+import threading
+from enum import Enum
+from typing import Optional, Union
+
+logging.addLevelName(logging.WARNING, "WARN")
+
+loglevel = os.environ.get("LOGLEVEL", "INFO").upper()
+
+_env_colors = os.getenv("LOGGING_COLORS")
+if _env_colors is not None:
+    use_colors = _env_colors.lower() == "true"
+else:
+    use_colors = sys.stderr.isatty()
+
+_env_log_caller = os.getenv("LOGGING_ALWAYS_LOG_CALLER")
+always_log_caller = _env_log_caller is not None and _env_log_caller.lower() == "true"
+
+
+class HandlerMode(Enum):
+    NOWHERE = 0
+    IN_TENSOR_PARALLEL = 1
+    IN_ROOT_LOGGER = 2
+
+
+_init_lock = threading.RLock()
+_current_mode = HandlerMode.IN_TENSOR_PARALLEL
+_default_handler = None
+
+
+class TextStyle:
+    """
+    ANSI escape codes. Details: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    """
+
+    RESET = "\033[0m"
+    BOLD = "\033[1m"
+    RED = "\033[31m"
+    BLUE = "\033[34m"
+    PURPLE = "\033[35m"
+    ORANGE = "\033[38;5;208m"  # From 8-bit palette
+
+    if not use_colors:
+        # Set the constants above to empty strings
+        _codes = locals()
+        _codes.update({_name: "" for _name in list(_codes) if _name.isupper()})
+
+
+class CustomFormatter(logging.Formatter):
+    """
+    A formatter that allows a log time and caller info to be overridden via
+    ``logger.log(level, message, extra={"origin_created": ..., "caller": ...})``.
+    """
+
+    # Details: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    _LEVEL_TO_COLOR = {
+        logging.DEBUG: TextStyle.PURPLE,
+        logging.INFO: TextStyle.BLUE,
+        logging.WARNING: TextStyle.ORANGE,
+        logging.ERROR: TextStyle.RED,
+        logging.CRITICAL: TextStyle.RED,
+    }
+
+    def format(self, record: logging.LogRecord) -> str:
+        if hasattr(record, "origin_created"):
+            record.created = record.origin_created
+            record.msecs = (record.created - int(record.created)) * 1000
+
+        if record.levelno != logging.INFO or always_log_caller:
+            if not hasattr(record, "caller"):
+                record.caller = f"{record.name}.{record.funcName}:{record.lineno}"
+            record.caller_block = f" [{TextStyle.BOLD}{record.caller}{TextStyle.RESET}]"
+        else:
+            record.caller_block = ""
+
+        # Aliases for the format argument
+        record.levelcolor = self._LEVEL_TO_COLOR[record.levelno]
+        record.bold = TextStyle.BOLD
+        record.reset = TextStyle.RESET
+
+        return super().format(record)
+
+
+def _initialize_if_necessary():
+    global _current_mode, _default_handler
+
+    with _init_lock:
+        if _default_handler is not None:
+            return
+
+        formatter = CustomFormatter(
+            fmt="{asctime}.{msecs:03.0f} [{bold}{levelcolor}{levelname}{reset}]{caller_block} {message}",
+            style="{",
+            datefmt="%b %d %H:%M:%S",
+        )
+        _default_handler = logging.StreamHandler()
+        _default_handler.setFormatter(formatter)
+
+        _enable_default_handler("tensor_parallel")
+
+
+def get_logger(name: Optional[str] = None) -> logging.Logger:
+    """
+    Same as ``logging.getLogger()`` but ensures that the default log handler is initialized.
+
+    :note: By default, the log handler (that reads the ``LOGLEVEL`` env variable and uses
+           the colored log formatter) is only applied to messages logged inside the package.
+           If you want to extend this handler to other loggers in your application, call
+           ``use_log_handler("in_root_logger")``.
+    """
+
+    _initialize_if_necessary()
+    return logging.getLogger(name)
+
+
+def _enable_default_handler(name: str) -> None:
+    logger = get_logger(name)
+    logger.addHandler(_default_handler)
+    logger.propagate = False
+    logger.setLevel(loglevel)
+
+
+def _disable_default_handler(name: str) -> None:
+    logger = get_logger(name)
+    logger.removeHandler(_default_handler)
+    logger.propagate = True
+    logger.setLevel(logging.NOTSET)
+
+
+def use_log_handler(where: Union[HandlerMode, str]) -> None:
+    """
+    Choose loggers where the default log handler is applied. Options for the ``where`` argument are:
+
+    * "in_tensor_parallel" (default): Use the log handler in the loggers of the ``tensor_parallel`` package.
+                               Don't propagate their messages to the root logger.
+    * "nowhere": Don't use the log handler anywhere.
+                 Propagate the ``tensor_parallel`` messages to the root logger.
+    * "in_root_logger": Use the tensor_parallel log handler in the root logger
+                        (that is, in all application loggers until they disable propagation to the root logger).
+                        Propagate the ``tensor_parallel`` messages to the root logger.
+
+    The options may be defined as strings (case-insensitive) or values from the HandlerMode enum.
+    """
+
+    global _current_mode
+
+    if isinstance(where, str):
+        # We allow `where` to be a string, so a developer does not have to import the enum for one usage
+        where = HandlerMode[where.upper()]
+
+    _initialize_if_necessary()
+
+    if where == _current_mode:
+        return
+
+    if _current_mode == HandlerMode.IN_TENSOR_PARALLEL:
+        _disable_default_handler("tensor_parallel")
+    elif _current_mode == HandlerMode.IN_ROOT_LOGGER:
+        _disable_default_handler(None)
+
+    _current_mode = where
+
+    if _current_mode == HandlerMode.IN_TENSOR_PARALLEL:
+        _enable_default_handler("tensor_parallel")
+    elif _current_mode == HandlerMode.IN_ROOT_LOGGER:
+        _enable_default_handler(None)
+
+
+def golog_level_to_python(level: str) -> int:
+    level = level.upper()
+    if level in ["DPANIC", "PANIC", "FATAL"]:
+        return logging.CRITICAL
+
+    level = logging.getLevelName(level)
+    if not isinstance(level, int):
+        raise ValueError(f"Unknown go-log level: {level}")
+    return level
+
+
+def python_level_to_golog(level: str) -> str:
+    if not isinstance(level, str):
+        raise ValueError("`level` is expected to be a Python log level in the string form")
+
+    if level == "CRITICAL":
+        return "FATAL"
+    if level == "WARNING":
+        return "WARN"
+    return level
---
+++ tests/test_integration.py
@@ -0,0 +1,58 @@
+import random
+
+import pytest
+import torch
+from transformers import BloomConfig
+from transformers.models.bloom.modeling_bloom import BloomBlock
+
+from tensor_parallel import Config, TensorParallel
+
+
+@pytest.mark.parametrize("custom_config", [True, False])
+@pytest.mark.parametrize("devices", [("cpu",) * 2, ("cpu",) * 3, ("cpu",) * 4])
+def test_tp_bloom_block(devices, custom_config):
+    bloom_config = BloomConfig.from_pretrained("bigscience/bloom-560m")
+    block = BloomBlock(bloom_config)
+
+    tp_config = None
+    if custom_config:
+        tp_config = Config(
+            state_rules={
+                r".*self_attention\.query_key_value\.(weight|bias)": "split 0",
+                r".*self_attention\.dense\.(weight|bias)": "split 0",
+                r".*mlp\.dense_h_to_4h\.(weight|bias)": "split 0",
+                r".*mlp\.dense_4h_to_h\.weight": "split 1",
+                r".*mlp\.dense_4h_to_h\.bias": "scale",
+            },
+            input_rules={},
+            output_rules={
+                ".*self_attention\.query_key_value": {0: "gather -1"},
+                ".*self_attention\.dense": {0: "gather -1"},
+                ".*mlp\.dense_4h_to_h$": {0: "sum"},
+            },
+            attr_rules={},
+        )
+
+    test_inputs1 = torch.randn(2, 3, 1024, requires_grad=True, device=devices[0])
+    test_inputs2 = test_inputs1.detach().clone().requires_grad_(True)
+    batch_size = test_inputs1.shape[0]
+    head_dim = len(block.input_layernorm.weight) // block.num_heads
+    prefix_length = 5
+
+    layer_past = (
+        torch.randn(batch_size * block.num_heads, head_dim, prefix_length, device=devices[0]),
+        torch.randn(batch_size * block.num_heads, prefix_length, head_dim, device=devices[0]),
+    )
+
+    grad_proj = torch.rand_like(test_inputs1)
+    y_ref, cache_ref = block(test_inputs1, use_cache=True, layer_past=layer_past)
+    y_ref.backward(grad_proj)
+
+    block_tp = TensorParallel(block, devices, config=tp_config)
+    y_ours, cache_ours = block_tp(test_inputs2, use_cache=True, layer_past=layer_past)
+    y_ours.backward(grad_proj)
+
+    assert torch.allclose(y_ours, y_ref, atol=1e-6)
+    assert torch.allclose(test_inputs1.grad, test_inputs2.grad, atol=1e-6)
+    assert torch.allclose(cache_ref[0], cache_ours[0], atol=1e-6)
+    assert torch.allclose(cache_ref[1], cache_ours[1], atol=1e-6)
