c06ce253093595dd32799f4dbadedf5a7e223936
Andrei Panferov <andrei@blacksamorez.ru>
2022-12-18 20:13:01 +0000

    delete

--- petals_local_parallel/__init__.py
+++
@@ -1,6 +0,0 @@
-from .slicer_wrapper_interface import SlicingConfig, tensor_parallel
-
-__all__ = [
-    "SlicingConfig",
-    "tensor_parallel"
-]
--- petals_local_parallel/slicing_config.py
+++
@@ -1,4 +0,0 @@
-class SlicingConfig():
-    def __init__(self, tensor_rules: dict, module_rules: dict):
-        self.tensor_rules = tensor_rules
-        self.module_rules = module_rules
