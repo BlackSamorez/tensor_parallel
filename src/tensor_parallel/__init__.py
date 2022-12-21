90e9dc920ac8e891521fd5ce2a7c40d8c3ea848a
Andrei Panferov <andrei@blacksamorez.ru>
2022-12-21 22:26:23 +0000

    __init__.py fix

--- src/tensor_parallel/__init__.py
+++ src/tensor_parallel/__init__.py
@@ -6,4 +6,4 @@ Splits linear, conv and some other layers between GPUs
 from tensor_parallel.factory import tensor_parallel
 from tensor_parallel.slicer_wrapper import Config
 from tensor_parallel.tensor_parallel import TensorParallel
-from tensor_parallel.tensor_parallel_pretrained_model import TensorParallelPreTrainedModel
+from tensor_parallel.pretrained_model import TensorParallelPreTrainedModel
