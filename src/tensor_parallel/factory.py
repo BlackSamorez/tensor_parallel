97b3e57e7faa059b8e8cb926db0f49c83973d38b
justheuristic <justheuristic@gmail.com>
2022-12-28 21:36:45 +0300

    use sharding properly

--- src/tensor_parallel/factory.py
+++ src/tensor_parallel/factory.py
@@ -67,7 +67,7 @@ def tensor_parallel(
                 module.tensor_parallel,
                 sharded,
                 model_size,
-                sharded_param_names,
+                sharded_param_names=sharded_param_names,
             )
         else:
             module = TensorParallel(module, device_ids=device_ids, config=config, **kwargs)
