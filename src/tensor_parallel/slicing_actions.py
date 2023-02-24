181825d6b482d61972841e9d17cb9e86d94271ea
Andrei Panferov <andrei@blacksamorez.ru>
2023-02-24 11:23:32 +0000

    slicing actions

---
+++ src/tensor_parallel/slicing_actions.py
@@ -0,0 +1,114 @@
+"""slicing_actions.py: file contains actions """
+
+import torch
+from torch import Tensor
+
+from abc import ABC, abstractclassmethod
+from typing import Any
+
+from tensor_parallel.communications import CollectiveOperation
+from tensor_parallel.tensor_parallel import PerDeviceTensors
+from tensor_parallel.slicer_wrapper import Action
+
+
+class Action(ABC):
+    @abstractclassmethod
+    def __call__(self, input: Any, rank: int) -> Any:
+        pass
+
+
+class Split(Action):
+    def __init__(self, world_size: int, dim: int):
+        super().__init__()
+        self.world_size = world_size
+        self.dim = dim
+
+    def __call__(self, input: Tensor, rank: int) -> Tensor:
+        return torch.tensor_split(input, self.world_size, dim=self.dim)[rank]
+
+
+class Scale(Action):
+    def __init__(self, world_size: int):
+        super().__init__()
+        self.world_size = world_size
+
+    def __call__(self, input: Tensor, rank: int) -> Tensor:
+        return input / self.world_size
+
+
+class SplitInChunks(Action):
+    def __init__(self, world_size: int, dim:int, chunk_size: int, optional: bool = False):
+        super().__init__()
+        self.world_size = world_size
+        self.dim = dim
+        self.chunk_size = chunk_size
+        self.optional = optional
+
+    def __call__(self, input: Tensor, rank: int) -> Tensor:
+        if input is None and self.optional:
+            return None
+        dim = self.dim
+        assert input.shape[dim] % self.chunk_size == 0, input.shape
+        if dim < 0:
+            dim = (input.ndim + dim) % input.ndim
+        shape = list(input.shape)
+        shape[dim] //= self.chunk_size
+        shape.insert(dim + 1, self.chunk_size)
+        tensor_part = input.reshape(shape).tensor_split(self.world_size, dim=dim)[rank].flatten(dim, dim + 1)
+        return tensor_part
+
+
+class SplitNumChunks(Action):
+    def __init__(self, world_size: int):
+        super().__init__()
+        self.world_size = world_size
+
+    def __call__(self, num_chunks: int, rank: int) -> int:
+        return torch.empty(num_chunks, device="meta").tensor_split(self.world_size)[rank].numel()
+
+
+class SplitDimInChunks(Action):
+    def __init__(self, world_size: int, num_chunks: int):
+        super().__init__()
+        self.world_size = world_size
+        self.num_chunks = num_chunks
+
+    def __call__(self, inner_dim: int, rank: int) -> int:
+        return torch.empty(self.num_chunks, device="meta").tensor_split(self.world_size)[rank].numel() * (inner_dim // self.num_chunks)
+
+
+class GatherKV(Action):
+    def __init__(self, world_size: int):
+        super().__init__()
+        self.world_size = world_size
+
+    def __call__(self, present_key_value_state, rank: int):
+        if present_key_value_state[0] is None:
+            return present_key_value_state
+        else:
+            return [tuple(PerDeviceTensors(*item) for item in present_key_value_state)] * self.world_size
+
+
+class SplitAlibi(Action):
+    def __init__(self, world_size: int, num_heads: int):
+        super().__init__()
+        self.world_size = world_size
+        self.num_heads = num_heads
+
+    def __call__(self, alibi: Tensor, rank: int) -> Tensor:
+        alibi_expanded = alibi.reshape(-1, self.num_heads, *alibi.shape[1:])
+        alibi_part = alibi_expanded.tensor_split(self.world_size, dim=1)[rank]
+        return alibi_part.reshape(-1, *alibi.shape[1:])
+
+
+def gather_kv(world_size: int) -> Action:
+    """Constructs an Action for gathering attention caches across ranks"""
+    def operation(*present_key_value_state):
+        if present_key_value_state[0] is None:
+            return present_key_value_state
+        else:
+            return [tuple(PerDeviceTensors(*item) for item in zip(*present_key_value_state))] * world_size
+
+    return CollectiveOperation(
+        world_size=world_size, func=operation
+    )
--- src/tensor_parallel/slicing_configs.py
+++ src/tensor_parallel/slicing_configs.py
@@ -13,6 +13,13 @@ import torch
 from transformers import BertConfig, BloomConfig, CodeGenConfig, GPT2Config, GPTNeoXConfig, PretrainedConfig, T5Config
 
 from tensor_parallel.communications import CollectiveOperation
+from tensor_parallel.slicing_actions import (
+    SplitInChunks,
+    SplitNumChunks,
+    SplitDimInChunks,
+    gather_kv,
+    SplitAlibi,
+)
 from tensor_parallel.slicer_wrapper import Config
 from tensor_parallel.tensor_parallel import PerDeviceTensors
 from tensor_parallel.utils import nested_map
@@ -20,36 +27,23 @@ from tensor_parallel.utils import nested_map
 ConfigGetter = Callable[[PretrainedConfig, Sequence[torch.device]], Config]
 
 
-def gather_kv(*present_key_value_state, world_size):
-    if present_key_value_state[0] is None:
-        return present_key_value_state
-    else:
-        return [tuple(PerDeviceTensors(*item) for item in zip(*present_key_value_state))] * world_size
-
-
-def select_kv_for_rank(present_key_value_state, rank):
-    return nested_map(lambda x: x[rank] if isinstance(x, PerDeviceTensors) else x, present_key_value_state)
-
-
 def get_bloom_config(model_config: BloomConfig, devices: Sequence[torch.device]) -> Config:
     world_size = len(devices)
     num_heads = model_config.n_head
     head_dim = model_config.hidden_size // num_heads
-    gather_kv_across_ranks = CollectiveOperation(
-        world_size=world_size, func=lambda *kvs: gather_kv(*kvs, world_size=world_size)
-    )  # this operation ensures that we get attention cache for all heads on each device
 
-    _split_alibi = partial(split_alibi, num_heads=num_heads, world_size=world_size)
+    def select_kv_for_rank(present_key_value_state, rank):
+        return nested_map(lambda x: x[rank] if isinstance(x, PerDeviceTensors) else x, present_key_value_state)
 
     return Config(
         state_rules={
             # BloomAttention
             r".*self_attention\.query_key_value\.(weight|bias)$": (
-                partial(split_heads, dim=0, head_dim=head_dim * 3, world_size=world_size),
+                SplitInChunks(world_size=world_size, dim=0, chunk_size=head_dim * 3),
                 "split 0",
             ),
             r".*self_attention\.dense\.weight$": (
-                partial(split_heads, dim=1, head_dim=head_dim, world_size=world_size),
+                SplitInChunks(world_size=world_size, dim=1, chunk_size=head_dim),
                 "split 1",
             ),
             r".*self_attention\.dense\.bias$": "scale",
@@ -62,61 +56,25 @@ def get_bloom_config(model_config: BloomConfig, devices: Sequence[torch.device])
             # note: ^-- lm_head.weight is tied with word_embeddings
         },
         input_rules={
-            r".*self_attention$": {"layer_past": select_kv_for_rank, "alibi": _split_alibi},
+            r".*self_attention$": {"layer_past": select_kv_for_rank, "alibi": SplitAlibi(world_size=world_size, num_heads=num_heads)},
             r".*lm_head$": {0: "split -1"},  # note: we need to split lm_head inputs because
             # ... lm_head's weights (tied embeddings) are already split across input dimension
         },
         output_rules={
-            r".*self_attention$": {1: gather_kv_across_ranks},
+            r".*self_attention$": {1: gather_kv(world_size=world_size)},
             r".*self_attention\.dense$": {0: "sum"},
             r".*mlp\.dense_4h_to_h$": {0: "sum"},
             r".*word_embeddings$": {0: "gather -1"},
             r".*lm_head$": {0: "sum"},
         },
-        attr_rules={r".*self_attention$": {"num_heads": partial(split_num_heads, world_size=world_size)}},
+        attr_rules={r".*self_attention$": {"num_heads": SplitNumChunks(world_size=world_size)}},
     )
-
-
-def split_heads(tensor: torch.Tensor, *, dim: int, head_dim: int, rank: int, world_size: int, optional: bool = False):
-    """Split a tensor along dim such that each part size is divisible by head_dim"""
-    if tensor is None and optional:
-        return None
-    assert tensor.shape[dim] % head_dim == 0, tensor.shape
-    if dim < 0:
-        dim = (tensor.ndim + dim) % tensor.ndim
-    shape = list(tensor.shape)
-    shape[dim] //= head_dim
-    shape.insert(dim + 1, head_dim)
-    tensor_part = tensor.reshape(shape).tensor_split(world_size, dim=dim)[rank].flatten(dim, dim + 1)
-    if tensor_part.shape[dim] == 0:
-        return torch.zeros(shape[:dim] + shape[dim + 1 :])
-    return tensor_part
-
-
-def split_num_heads(num_heads: int, *, rank: int, world_size: int):
-    assigned_num_heads = torch.empty(num_heads, device="meta").tensor_split(world_size)[rank].numel()
-    return assigned_num_heads if assigned_num_heads != 0 else 1
-
-
-def split_inner_dim(inner_dim: int, *, rank: int, num_heads: int, world_size: int):
-    return split_num_heads(num_heads=num_heads, rank=rank, world_size=world_size) * (inner_dim // num_heads)
-
-
-def split_alibi(alibi: torch.Tensor, *, rank: int, num_heads: int, world_size: int) -> torch.Tensor:
-    """split alibi tensor of shape [batch_size * num_heads, ...] over attention heads"""
-    alibi_expanded = alibi.reshape(-1, num_heads, *alibi.shape[1:])
-    alibi_part = alibi_expanded.tensor_split(world_size, dim=1)[rank]
-    return alibi_part.reshape(-1, *alibi.shape[1:])
 
 
 def get_t5_config(model_config: T5Config, devices: Sequence[torch.device]) -> Config:
     world_size = len(devices)
     num_heads = model_config.num_heads
     head_dim = model_config.d_kv
-
-    gather_kv_across_ranks = CollectiveOperation(
-        world_size=world_size, func=lambda *kvs: gather_kv(*kvs, world_size=world_size)
-    )  # this operation ensures that we get attention cache for all heads on each device
 
     def select_kv_for_rank(*kvs, rank):
         if kvs[0] is None:
@@ -136,20 +94,20 @@ def get_t5_config(model_config: T5Config, devices: Sequence[torch.device]) -> Co
         state_rules={
             # T5Attention
             r".*SelfAttention\.q\.(weight|bias)$": (
-                partial(split_heads, dim=0, head_dim=head_dim, world_size=world_size),
+                SplitInChunks(world_size=world_size, dim=0, chunk_size=head_dim),
                 "split 0",
             ),
             r".*SelfAttention\.k\.(weight|bias)$": (
-                partial(split_heads, dim=0, head_dim=head_dim, world_size=world_size),
+                SplitInChunks(world_size=world_size, dim=0, chunk_size=head_dim),
                 "split 0",
             ),
             r".*SelfAttention\.v\.(weight|bias)$": (
-                partial(split_heads, dim=0, head_dim=head_dim, world_size=world_size),
+                SplitInChunks(world_size=world_size, dim=0, chunk_size=head_dim),
                 "split 0",
             ),
             r".*relative_attention_bias\.weight$": "split 1",
             r".*SelfAttention\.o\.weight$": (
-                partial(split_heads, dim=1, head_dim=head_dim, world_size=world_size),
+                SplitInChunks(world_size=world_size, dim=1, chunk_size=head_dim),
                 "split 1",
             ),
             # T5DenseGatedActDense
@@ -169,17 +127,17 @@ def get_t5_config(model_config: T5Config, devices: Sequence[torch.device]) -> Co
             # ... lm_head's weights (tied embeddings) are already split across input dimension
         },
         output_rules={
-            r".*SelfAttention$": {0: "sum", 1: gather_kv_across_ranks},
+            r".*SelfAttention$": {0: "sum", 1: gather_kv(world_size=world_size)},
             r".*DenseReluDense$": {0: "sum"},
             r".*shared$": {0: "gather -1"},
             r".*lm_head$": {0: "sum"},
         },
         attr_rules={
             r".*SelfAttention$": {
-                "n_heads": partial(split_num_heads, world_size=world_size),
-                "inner_dim": partial(split_inner_dim, num_heads=model_config.num_heads, world_size=world_size),
+                "n_heads": SplitNumChunks(world_size=world_size),
+                "inner_dim": SplitDimInChunks(world_size=world_size, num_chunks=num_heads),
             },
-            r".*relative_attention_bias$": {"embedding_dim": partial(split_num_heads, world_size=world_size)},
+            r".*relative_attention_bias$": {"embedding_dim": SplitDimInChunks(world_size=world_size, num_chunks=num_heads)},
         },
     )
 
