5fa1e4d3ba370625d062e60ea0392e433a249a62
Andrei Panferov <andrei@blacksamorez.ru>
2023-03-10 18:05:23 +0000

    Keeping up with source

--- src/tensor_parallel/slicing_configs.py
+++ src/tensor_parallel/slicing_configs.py
@@ -352,9 +352,9 @@ def get_llama_config(model_config: LLaMAConfig, devices: Sequence[torch.device])
                 "split 1",
             ),
             # LLaMAFeedForward
-            r".*feed_forward\.w1\.weight$": "split 0",
-            r".*feed_forward\.w2\.weight$": "split 1",
-            r".*feed_forward\.w3\.weight$": "split 0",
+            r".*mlp\.gate_proj\.weight$": "split 0",
+            r".*mlp\.down_proj\.weight$": "split 1",
+            r".*mlp\.up_proj\.weight$": "split 0",
             # LLaMAModel
             r".*embed_tokens.weight$": "split 1",
             r".*lm_head\.weight$": "split 0",
@@ -364,7 +364,7 @@ def get_llama_config(model_config: LLaMAConfig, devices: Sequence[torch.device])
         },
         output_rules={
             r".*self_attn$": {0: "sum", 2: gather_kv_across_ranks},
-            r".*feed_forward$": {0: "sum"},
+            r".*mlp$": {0: "sum"},
             r".*embed_tokens$": {0: "gather -1"},
             r".*lm_head$": {0: "gather -1"},
         },
