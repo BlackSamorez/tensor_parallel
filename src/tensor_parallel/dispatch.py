8c7995e78f4e2af9538560d3d0926d7662cf7af8
Andrei Panferov <andrei@blacksamorez.ru>
2023-03-26 18:53:36 +0000

    small refactoring

--- src/tensor_parallel/dispatch.py
+++ src/tensor_parallel/dispatch.py
@@ -50,7 +50,15 @@ def infer_sharded_data_device_id(name: str):
     return int(name[shard_id_start:shard_id_end]) if shard_id_end > 0 else int(name[shard_id_start:])
 
 
-def infer_sharded_device_map(tp_model):
+def infer_sharded_device_map(tp_model: Union[TensorParallel, TensorParallelPreTrainedModel]) -> dict:
+    """Creates a mapping describing which device each tensor parallel model parameter should be put on.
+
+    Args:
+        tp_model (Union[TensorParallel, TensorParallelPreTrainedModel]): tensor parallel model
+
+    Returns:
+        dict: parameter device mapping
+    """
     device_map = {}
     for name, _ in tp_model.named_parameters():
         device_map[name] = tp_model.devices[infer_sharded_data_device_id(name)]
@@ -59,10 +67,25 @@ def infer_sharded_device_map(tp_model):
     return device_map
 
 
-def convert_state_dict(input_state_dict, config: Config, world_size: int, for_pretrained: bool = False) -> dict:
+def convert_state_dict(
+    input_state_dict, tensor_parallel_config: Config, world_size: int, for_pretrained: bool = False
+) -> dict:
+    """Creates a state_dict to be loaded into a tensor parallel model from a state_dict of a base model.
+
+    Args:
+        input_state_dict (_type_): state_dict to be converted
+        tensor_parallel_config (Config): tensor parallel config for the base model
+        world_size (int): number of devices of the tensor parallel model
+        for_pretrained (bool, optional): Whether to create a state dict to be loaded into TensorParallelModel of PreTrainedTensorParallelModel. Defaults to False.
+
+    Returns:
+        dict: _description_
+    """
     output_state_dict = {}
     for i in range(world_size):
-        output_state_dict.update(convert_state_dict_single_shard(input_state_dict, config, world_size, i))
+        output_state_dict.update(
+            convert_state_dict_single_shard(input_state_dict, tensor_parallel_config, world_size, i)
+        )
         output_state_dict[f"_sanity_check_params.{i}"] = torch.empty(0, device="cpu")
     if for_pretrained:
         name_replacements = {name: "wrapped_model." + name for name in output_state_dict.keys()}
@@ -71,9 +94,9 @@ def convert_state_dict(input_state_dict, config: Config, world_size: int, for_pr
     return output_state_dict
 
 
-def convert_data(input_state_dict, output_state_dict, config: Config, world_size: int, rank: int):
+def convert_data(input_state_dict, output_state_dict, tensor_parallel_config: Config, world_size: int, rank: int):
     for name, state in input_state_dict.items():
-        for pattern, action in config.state_rules.items():
+        for pattern, action in tensor_parallel_config.state_rules.items():
             if pattern.search(name) is not None:
                 output_state_dict[name] = apply_action(state, action, rank=rank, world_size=world_size)
                 break
@@ -81,8 +104,11 @@ def convert_data(input_state_dict, output_state_dict, config: Config, world_size
             output_state_dict[name] = input_state_dict[name]  # copy source parameter as is
 
 
-def convert_names(state_dict, config: Config):
-    patterns = tuple(regex.pattern for regex in chain(config.input_rules.keys(), config.output_rules.keys()))
+def convert_names(state_dict, tensor_parallel_config: Config):
+    patterns = tuple(
+        regex.pattern
+        for regex in chain(tensor_parallel_config.input_rules.keys(), tensor_parallel_config.output_rules.keys())
+    )
     patterns = set(pattern[:-1] + "\." if pattern.endswith("$") else pattern for pattern in patterns)
     patterns = [re.compile(pattern) for pattern in patterns]
 
@@ -105,9 +131,9 @@ def prefix_names_with_shard_id(state_dict, rank: int):
         state_dict[new_name] = state_dict.pop(old_name)
 
 
-def convert_state_dict_single_shard(input_state_dict, config: Config, world_size: int, rank: int):
+def convert_state_dict_single_shard(input_state_dict, tensor_parallel_config: Config, world_size: int, rank: int):
     output_state_dict = {}
-    convert_data(input_state_dict, output_state_dict, config, world_size, rank)
-    convert_names(output_state_dict, config)
+    convert_data(input_state_dict, output_state_dict, tensor_parallel_config, world_size, rank)
+    convert_names(output_state_dict, tensor_parallel_config)
     prefix_names_with_shard_id(output_state_dict, rank)
     return output_state_dict
